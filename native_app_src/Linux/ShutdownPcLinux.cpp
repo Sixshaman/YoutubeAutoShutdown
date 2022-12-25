9db406f37ba9f0a939480b3e3bba48a15bdd15c6
Sixshaman <Shaman1234@gmail.com>
2022-12-25 17:03:40 +0300

    Refactored the shutdown timer dialog code on Linux

--- native_app_src/Linux/ShutdownPcLinux.cpp
+++ native_app_src/Linux/ShutdownPcLinux.cpp
@@ -9,32 +9,43 @@ void systemShutdown()
     [[maybe_unused]] int ret = system("shutdown -P now");
 }
 
+using TimePoint = decltype(std::chrono::high_resolution_clock::now());
+
+struct DialogTimerTextParams
+{
+    GtkMessageDialog* Dialog;
+    TimePoint         StartTime;
+    guint             TimerId;
+};
+
+void destroyTimerParams(gpointer timerParamsPtr)
+{
+    DialogTimerTextParams* timerParams = reinterpret_cast<DialogTimerTextParams*>(timerParamsPtr);
+    g_source_remove(timerParams->TimerId);
+    delete timerParams;
+}
+
 void onAppActivated(GtkApplication* app, [[maybe_unused]] gpointer userData)
 {
-    using TimePoint             = decltype(std::chrono::high_resolution_clock::now());
-    using TimeoutCallbackParams = std::tuple<GtkMessageDialog*, TimePoint>;
-
-    void* timeoutParamsStorage = malloc(sizeof(TimeoutCallbackParams));
-
     GtkWindow* tmpWindow = GTK_WINDOW(gtk_application_window_new(app));
     GtkWidget* messageDialog = gtk_message_dialog_new(tmpWindow, GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CANCEL, "Shutting down in %d seconds...", 10);
 
-    TimeoutCallbackParams* timeoutParams = new(timeoutParamsStorage) TimeoutCallbackParams(std::make_tuple(GTK_MESSAGE_DIALOG(messageDialog), std::chrono::high_resolution_clock::now()));
-    g_timeout_add(100, [](gpointer userData)
-    {
-        TimeoutCallbackParams params = *static_cast<TimeoutCallbackParams*>(userData);
+    DialogTimerTextParams* timeoutParams = new DialogTimerTextParams();
+    timeoutParams->Dialog = GTK_MESSAGE_DIALOG(messageDialog);
+    timeoutParams->StartTime = std::chrono::high_resolution_clock::now();
 
-        GtkMessageDialog*       messageDialog = std::get<0>(params);
-        std::chrono::time_point startTime     = std::get<1>(params);
+    timeoutParams->TimerId = g_timeout_add(100, [](gpointer userData)
+    {
+        DialogTimerTextParams* timerParams = reinterpret_cast<DialogTimerTextParams*>(userData);
 
         TimePoint currTime = std::chrono::high_resolution_clock::now();
-        int64_t secondsPassed = std::chrono::duration_cast<std::chrono::seconds>(currTime - startTime).count();
+        int64_t secondsPassed = std::chrono::duration_cast<std::chrono::seconds>(currTime - timerParams->StartTime).count();
 
         int secondsLeft = 10 - (int)secondsPassed;
         if(secondsLeft >= 0)
         {
             std::string newText = "<span size=\"x-large\">Shutting down in " + std::to_string(secondsLeft) + " seconds...</span>";
-            gtk_message_dialog_set_markup(messageDialog, newText.c_str());
+            gtk_message_dialog_set_markup(timerParams->Dialog, newText.c_str());
         }
         else
         {
@@ -47,9 +58,10 @@ void onAppActivated(GtkApplication* app, [[maybe_unused]] gpointer userData)
 
     gtk_window_set_transient_for(GTK_WINDOW(messageDialog), tmpWindow);
 
-    g_signal_connect(messageDialog, "response", G_CALLBACK(gtk_window_destroy), nullptr);
+    g_signal_connect_swapped(messageDialog, "response", G_CALLBACK(destroyTimerParams), timeoutParams);
+    g_signal_connect_swapped(messageDialog, "response", G_CALLBACK(gtk_window_destroy), messageDialog);
     g_signal_connect_swapped(messageDialog, "response", G_CALLBACK(gtk_window_destroy), tmpWindow);
-    g_signal_connect_swapped(messageDialog, "response", G_CALLBACK(free), timeoutParamsStorage);
+
     gtk_widget_show(messageDialog);
 }
 
